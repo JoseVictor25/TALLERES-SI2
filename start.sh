8f5285b41b1fb4745c6b3e5dc2e9ccaf4e4a6437
Jose Ugarteche <jvua2001.jvua@gmail.com>
2026-06-03 11:23:26 -0400

    miercoles 2

--- start.sh
+++ start.sh
@@ -1,11 +1,5 @@
 #!/usr/bin/env bash
 
-# Render provee la base de datos con el formato postgres://
-# SQLAlchemy asíncrono y síncrono requieren un prefijo ligeramente diferente.
-# Reemplazamos "postgres://" por "postgresql://" y "postgresql+asyncpg://" en tiempo de ejecución.
-
-export SYNC_DATABASE_URL=$(python -c "import os; print(os.environ['RENDER_DB_URL'].replace('postgres://', 'postgresql://', 1))")
-export DATABASE_URL=$(python -c "import os; url=os.environ['RENDER_DB_URL'].replace('postgres://', 'postgresql://', 1); print(url.replace('postgresql://', 'postgresql+asyncpg://', 1))")
 
 echo "Ejecutando migraciones de Alembic..."
 alembic upgrade head
