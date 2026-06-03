d20e6a863ae01648bc99f7a465817dfed7ded232
Jose Ugarteche <jvua2001.jvua@gmail.com>
2026-06-02 22:40:27 -0400

    jose

--- start.sh
+++ start.sh
@@ -10,6 +10,12 @@ export DATABASE_URL=$(python -c "import os; url=os.environ['RENDER_DB_URL'].repl
 echo "Ejecutando migraciones de Alembic..."
 alembic upgrade head
 
+echo "Ejecutando scripts para inicializar datos base..."
+python seed_roles.py
+python seed_especialidades.py
+python seed_tipos_incidente.py
+python seed_diagnostico.py
+
 echo "Iniciando servidor Uvicorn..."
 # Render provee el puerto automáticamente a través de la variable $PORT
 uvicorn app.main:app --host 0.0.0.0 --port $PORT
